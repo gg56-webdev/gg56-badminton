445703c8145c94e9c907eeef61e13531d75546fa
Nikita Liakhovka <n.liakhovka@gmail.com>
2024-05-30 17:47:23 +0900

    wip

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        bumblebee: {
+          ...require("daisyui/src/theming/themes").bumblebee,
+          primary: "orange",
+          "--rounded-btn": "0px",
+        },
+      },
+    ],
+  },
+};
